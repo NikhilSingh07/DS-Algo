973e15007bb0df52d90d35857758d8c2cfd562df
Nikhil Singh <msit.nikhil@gmail.com>
2023-04-17 00:10:57 +0100

    Level order Traversal

---
+++ BinaryTree/LevelOrderTraversal/Solution.java
@@ -0,0 +1,88 @@
+package BinaryTree.LevelOrderTraversal;
+import java.util.*;
+
+public class Solution {
+
+    static Scanner sc;
+
+    public static void main(String[] args) {
+
+        Node root = createTree();
+
+        int height = getHeight(root);
+       // System.out.println("height of the tree is: "+height);
+
+        printLevelOrder(root, height);      // Time complexity is O(n^2) using recursion.
+    }
+
+    static void printLevelOrder(Node root, int height){   // T(n) = O(n^2)
+
+
+        if(height == 0){
+            System.out.println("empty tree");         // O(1)
+        }
+        
+        for(int i=1 ;i <= height; i++) {        
+
+            getCurrentLevel(root, i);          //O(n)*O(n)
+        }
+
+
+    }
+
+    static void getCurrentLevel(Node root, int level){    // T(n) = 2T(n/2) + 1 -> O(n) [Master theroem]
+
+        if(root == null)
+        return;
+
+        if(level == 1) 
+        System.out.print(root.data+" ");             // O(1)
+
+        else {
+
+            getCurrentLevel(root.left, level-1);          // T(n/2)
+            getCurrentLevel(root.right, level-1);         // T(n/2)
+            System.out.println();                     // O(1)
+        }
+
+
+    }
+    static int getHeight(Node root) {
+
+        if(root == null)
+        return 0 ;
+
+        return Math.max(getHeight(root.left), getHeight(root.right))+1;
+
+    }
+
+    static Node createTree() {
+
+        Node root;
+        sc = new Scanner(System.in);
+        int data = sc.nextInt();
+
+        if(data == -1) return null;
+
+        root = new Node(data);
+        System.out.println("Enter left Node");
+        root.left = createTree();
+        System.out.println("Enter right Node");
+        root.right = createTree();
+
+        return root;
+
+    }
+}
+
+class Node{
+
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+
