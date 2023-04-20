bf103235ab8446ae2f1e689846a047cb16507aca
Nikhil Singh <msit.nikhil@gmail.com>
2023-04-20 17:13:53 +0100

    LC Intersection of 2 Arrays P1: HASHMAP: TC: O(n)

---
+++ Hashing/IntersectionOf2ArraysPart1/Solution.java
@@ -0,0 +1,34 @@
+package Hashing.IntersectionOf2ArraysPart1;
+import java.util.*;
+
+//LEETOCODE-> https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+        Set<Integer> set = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int arr[];
+
+        for(int x: nums1) {
+            set.add(x);
+        }
+
+        for(int x: nums2) {
+            if(set.contains(x)) {               
+                set.remove(x);
+                ans.add(x);
+            }
+        }
+
+        arr = new int[ans.size()];
+
+        for(int i=0; i<ans.size(); i++) {
+
+            arr [i] = (int)ans.get(i);
+        }
+        return arr;
+    }
+
+}
