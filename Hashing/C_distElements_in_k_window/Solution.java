a24818e2e38c9fcc053d306d5fbf6294eb6ab678
Nikhil Singh <msit.nikhil@gmail.com>
2023-04-23 12:45:18 +0100

    try 1

---
+++ Hashing/C_distElements_in_k_window/Solution.java
@@ -0,0 +1,7 @@
+package Hashing.C_distElements_in_k_window;
+
+public class Solution {
+    public static void main(String []args) {
+        System.out.println("hello");
+    }
+}
---
+++ Hashing/Count_dist_elmnts_in_every_window_of_size_k/Solution.java
@@ -0,0 +1,5 @@
+package Hashing.Count_dist_elmnts_in_every_window_of_size_k;
+
+public class Solution {
+    
+}
