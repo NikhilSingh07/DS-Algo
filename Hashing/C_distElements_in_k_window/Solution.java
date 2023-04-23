9d564b2f3a52b7ded820fa6ba9dc0847ea43edc2
Nikhil Singh <msit.nikhil@gmail.com>
2023-04-23 13:41:06 +0100

    Count the distinct elements in window of size k: 1) O(N*K^2)

--- Hashing/C_distElements_in_k_window/Solution.java
+++ Hashing/C_distElements_in_k_window/Solution.java
@@ -1,7 +1,39 @@
 package Hashing.C_distElements_in_k_window;
+import java.util.*;
 
 public class Solution {
     public static void main(String []args) {
-        System.out.println("hello");
+       
+        int arr[] = {1, 2, 1, 3, 4, 2, 3}, k = 4;
+        int n = 8;
+        countDist(arr,n,k);
+    }
+
+    static void countDist(int arr[], int n, int k) {  //O(n)*k^2
+
+        for(int i=0; i<n-k; i++) {  //O(n)
+            
+            countUtil(Arrays.copyOfRange(arr, i, k+i), k); 
+        }
+    }
+
+    static void countUtil(int arr[], int k) {
+        
+        int distCount = 0;
+        for(int i=0; i<k; i++) {       // O(k)
+            int j;
+            for (j= 0; j<=i; j++) {       //O(k)
+               
+                if(arr[i] == arr[j])
+                break;
+            }
+
+            if( i ==j) {
+                distCount ++;
+            }
+          
+        }
+
+        System.out.println(distCount);
     }
 }
